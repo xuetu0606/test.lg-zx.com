c27713cceec4cc7c7630bdf8cc9ebcbfa2616fff
yangbolin <287643563@qq.com>
2017-02-23 17:39:12 +0800

    edit error daili_model.php

--- application/views/daili/hytj.php
+++ application/views/daili/hytj.php
@@ -47,8 +47,8 @@
                                         <td><?= $item['gong'] ?></td>
                                         <td><?= $item['is_vip']?'VIP':'普通会员' ?></td>
                                         <td><?= $item['referrer']?$item['referrer']:'零工在线' ?></td>
-                                        <td><?= data('Y-m-d',$item['addtime']) ?></td>
-                                        <td><?= data('Y-m-d',$item['vip_starttime']) ?></td>
+                                        <td><?= date('Y-m-d',$item['addtime']) ?></td>
+                                        <td><?= date('Y-m-d',$item['vip_starttime']) ?></td>
                                         <td><?= $item['is_vip']?data('Y-m-d',$item['is_vip']):'无' ?></td>
                                         <td><?= $item['mobile'] ?></td>
                                     </tr>
