7329cca51799c2461450a8be6f87ceb4f88d51b9
xuetu <gexingfeng0606@163.com>
2017-03-16 14:20:03 +0800

    表单

--- application/views/admin/add-member.php
+++ application/views/admin/add-member.php
@@ -7,94 +7,45 @@
             </h1>
             <ol class="breadcrumb">
                 <li><a href="/admin"><i class="fa fa-dashboard"></i> 首页</a></li>
-                <li><a href="/admin/reveal">招聘展示</a></li>
+                <li><a href="/admin/addMember">会员添加</a></li>
             </ol>
         </section>
                         <!-- /.box-header -->
                         <div class="box-body">
-                            <table class="table table-bordered table-hover dataTable" width="50%" border="0" align="center" cellpadding="0" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>标题</th>
-                                        <th>工种</th>
-                                        <th>区/县</th>
-                                        <th>工资</th>
-                                        <th>薪资单位</th>
-                                        <th>结算周期</th>
-                                        <th>招聘人数</th>
-                                        <th>工作时间</th>
-                                        <th>审核</th>
-                                        <th>浏览量</th>
-                                        <th>添加时间</th>
-                                        <th>更新时间</th>
-                                        <th>刷新时间</th>
-                                        <th>工作内容</th>
-                                        <th>操作</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                if(count($users)>0) {
-                                    foreach ($users as $item) {
-                                        ?>
-                                        <tr>
-                                            
-                                            <td><?= $item['title'] ?></td>
-                                            <td><?= $item['job_name'] ?></td>
-                                            <td><?= $item['district_name'] ?></td>
-                                            <td><?= $item['pay'] ?></td>
-                                            <td><?= $item['pay_unit_name'] ?></td>
-                                            <td><?= $item['pay_circle_name'] ?></td>
-                                            <td><?= $item['sum'] ?></td>
-                                            <td><?= $item['worktime'] ?></td>
-                                            <td><?php 
-                                                    if($item['flag'] == 1){
-                                                        echo '审核通过';
-                                                    }else if($item['flag'] == -1){
-                                                        echo '审核失败';
-                                                    }else{
-                                                        echo '待审核';
-                                                    }
-                                                ?>
-                                            </td>
-                                            <td><?= $item['pv'] ?></td>
-                                            <td><?= date("Y-m-d",$item['addtime']) ?></td>
-                                            <td><?= date("Y-m-d",$item['updatetime']) ?></td>
-                                            <td><?= date("Y-m-d",$item['flushtime']) ?></td>
-                                            <td><?= $item['info'] ?></td>
-                                            <td>
-                                                <a href="<?php echo site_url('admin/toUpdateReveal'); ?>/<?= $item['id'] ?>">修改</a>
-                                                |
-                                                <a href="<?php echo site_url('admin/deleteReveal'); ?>/<?= $item['id'] ?>">删除</a>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                    }
-                                }else{
-                                    echo '<tr><td>暂无数据显示!</td></tr>';
-                                }
-                                ?>
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th>标题</th>
-                                        <th>工种</th>
-                                        <th>区/县</th>
-                                        <th>工资</th>
-                                        <th>薪资单位</th>
-                                        <th>结算周期</th>
-                                        <th>招聘人数</th>
-                                        <th>工作时间</th>
-                                        <th>审核</th>
-                                        <th>浏览量</th>
-                                        <th>添加时间</th>
-                                        <th>更新时间</th>
-                                        <th>刷新时间</th>
-                                        <th>工作内容</th>
-                                        <th>操作</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
+                            <div id="gongsi">
+                                <label for="">用户名:</label><input type="text" name=""/>
+                                <label for="">密码:</label><input type="text" name=""/>
+                                <label for="">省份:</label><input type="text" name=""/>
+                                <label for="">城市:</label><input type="text" name=""/>
+                                <label for="">详细地址:</label><input type="text" name=""/>
+                                <label for="">手机:</label><input type="text" name=""/>
+                                <label for="">固话:</label><input type="text" name=""/>
+                                <label for="">邮箱:</label><input type="text" name=""/>
+                                <label for="">QQ号:</label><input type="text" name=""/>
+                                <label for="">微信号:</label><input type="text" name=""/>
+                                <label for="">公司名称:</label><input type="text" name=""/>
+                                <label for="">公司规模:</label><input type="text" name=""/>
+                                <label for="">公司网址:</label><input type="text" name=""/>
+                                <label for="">证件号:</label><input type="text" name=""/>
+                            </div>
+                            <div id="geren">
+                                <label for="">用户名:</label><input type="text" name=""/>
+                                <label for="">密码:</label><input type="text" name=""/>
+                                <label for="">省份:</label><input type="text" name=""/>
+                                <label for="">城市:</label><input type="text" name=""/>
+                                <label for="">详细地址:</label><input type="text" name=""/>
+                                <label for="">手机:</label><input type="text" name=""/>
+                                <label for="">固话:</label><input type="text" name=""/>
+                                <label for="">邮箱:</label><input type="text" name=""/>
+                                <label for="">QQ号:</label><input type="text" name=""/>
+                                <label for="">微信号:</label><input type="text" name=""/>
+                                <label for="">真实姓名:</label><input type="text" name=""/>
+                                <label for="">昵称:</label><input type="text" name=""/>
+                                <label for="">男/女:</label><input type="text" name=""/>
+                                <label for="">身份证号:</label><input type="text" name=""/>
+                                <label for="">个人简介:</label><input type="text" name=""/>
+                            </div>
+                            
                             <div class="row">
 
                                 <div class="col-sm-7">
