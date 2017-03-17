ccf5f6ffc51b5374abe0dc5fced63e6a34581c8b
yangbolin <287643563@qq.com>
2017-03-17 15:23:00 +0800

    调试

--- application/views/home/user/lgb.php
+++ application/views/home/user/lgb.php
@@ -46,10 +46,10 @@
                                 </h3>
                             </div>
                             <div class="r">
-                                <a href="">刷新</a>
-                                <a href="/pub/edit">修改</a>
+                                <a href="<?php echo site_url('user/dealMyInfo/type/gz/ac/flush/id/'.$k['id'])?>">刷新</a>
+                                <a href="/pub/edit/<?php echo $k['id']?>">修改</a>
                                 <a href="/pub/edit">下架</a>
-                                <a href="/pub/del" class="delete">删除</a>
+                                <a href="javascript:void(0);" onclick="if(confirm('确定要删除吗？')){window.location.href='<?php echo site_url('user/dealMyInfo/type/gz/ac/del/id/'.$k['id'])?>';}else{return false;}" class="delete">删除</a>
                             </div>
                         </div>
                     </div>
