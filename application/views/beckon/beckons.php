c3cce2a313a937f098e59420efb61b5ec3bffbe1
xuetu <gexingfeng0606@163.com>
2017-03-24 17:22:39 +0800

    工种

--- application/views/beckon/beckons.php
+++ application/views/beckon/beckons.php
@@ -327,7 +327,7 @@
                         '" alt="" class="tx">'+
                         '<div class="jieshao">'+
                             '<div class="line1">'+
-                                '<a href="javascript:void(0);" class="name">'+
+                                '<a href="'<?php echo site_url('beckon/find');?>+'/'+data[i].id+'" class="name">'+
                                     data[i].title+
                                 '</a>'+
                                 '<span class="vip">'+
