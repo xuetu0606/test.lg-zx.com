b7a19253117c2ec522ba81868d6e6f6594164868
xuetu <gexingfeng0606@163.com>
2017-03-25 09:18:44 +0800

    JS

--- application/controllers/News_file.php
+++ application/controllers/News_file.php
@@ -1,16 +1,11 @@
 <?php
-	class Beckon extends CI_Controller {
+	class News_file extends CI_Controller {
 		public function __construct(){
 			parent::__construct();
 			$this->load->helpers('url');
 			$this->load->library('session');
 			$this->load->library('pagination');
 		}
-		/**
-		 * 打开消息文件页面
-		 */
-		public function index(){
-			
-		}
+		
 	}
 ?>
